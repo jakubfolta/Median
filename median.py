d546f1d8079fcb118f4131940f35b8c7a2cd909c
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-12-04 06:18:24 +0100

    finished solution

--- median.py
+++ median.py
@@ -1,2 +1,14 @@
-def median(numbers):
+def chooseMedian(numbers):
+    numbers = sorted(numbers)
+    print(numbers)
+    lengthNum = len(numbers)
+    if lengthNum % 2 == 0:
+        median = (numbers[int(lengthNum / 2)] + numbers[int(lengthNum / 2 - 1)]) / 2.0
+    else:
+        median = numbers[int(lengthNum / 2)]
+    return median
+
     
+print(chooseMedian([1, 2, 3, 8, 5, 2, 6, 5, 9, 4, 1, 2]))
+    
+  
