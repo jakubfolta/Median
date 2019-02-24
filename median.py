b214e013cd171476eef968ccb01d134641356cfd
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-02-24 12:08:42 +0100

    solidifying knowledge

--- median.py
+++ median.py
@@ -11,28 +11,6 @@ def chooseMedian(numbers):
     
 print(chooseMedian([1, 2, 3, 8, 5, 2, 6, 5, 9, 4, 1, 2]))
 
-def countMedian(numbers):
-    numbers = sorted(numbers)
-    print(len(numbers))
-    print((numbers[int(len(numbers) / 2 - 1)] + numbers[int(len(numbers) / 2)] / 2.0))
-    print(numbers[int(len(numbers) / 2 - 1)])
-    if len(numbers) % 2 == 0:
-        print('Median from this list is a ' + str(numbers[int(len(numbers) / 2 - 1)] + numbers[int(len(numbers) / 2)] / 2.0)) 
-
-countMedian([1, 2, 3, 5, 7, 4, 3, 4])
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 def count_median(numbers):
     numbers = sorted(numbers)
@@ -42,7 +20,7 @@ def count_median(numbers):
     else:
         print(numbers[int(len(numbers) // 2)])
 
-count_median([1, 2, 3, 5, 7, 4, 3, 4])
+count_median([1, 2, 3, 5, 7, 4, 3, 1, 4])
 
 
 
